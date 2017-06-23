7609f790061a602f538e4e2a47b01b373bb99e81
Jeff Epler <jepler@unpythonic.net>
2017-06-23 13:41:10 -0500

    do.sh: build 32-bit and 64-bit; get rid of dependencies on local environment

--- do.sh
+++ do.sh
@@ -3,6 +3,22 @@
 if [ $(id -u) -ne 0 ]; then exec sudo bash "$0" "$@"; fi
 
 set -eo pipefail
-cd /vms/lw
-rm -f live.iso
-time env http_proxy=http://localhost:8001/ lwr -t live-task-xfce -e "linux-image-rt-amd64 linux-headers-rt-amd64 firmware-linux" --description="Unofficial LinuxCNC 'Stretch' Live"
+
+LOCATION="$(dirname $(readlink -f "$0"))"
+cd "$LOCATION"
+
+if [ -e env.sh ] ; then . ./env.sh ; fi
+
+if [ $# -eq 0 ] ; then set -- i386 amd64; fi
+for ARCH in "$@"; do
+    case "$ARCH" in
+        i?86) KARCH=686-pae ;;
+        *) KARCH="$ARCH" ;;
+    esac
+    TARGET="$LOCATION/linuxcnc-stretch-uspace-$ARCH.iso"
+    lwr -o "$TARGET" \
+        --architecture=$ARCH \
+        -t live-task-xfce \
+        -e "linux-image-rt-$KARCH linux-headers-rt-$KARCH firmware-linux" \
+        --description="Unofficial LinuxCNC 'Stretch' $ARCH Live/Install"
+done
