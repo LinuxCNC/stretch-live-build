8ee8a9b9ce71073f7e51ad66bc317690db424d95
Jeff Epler <jepler@unpythonic.net>
2017-06-30 11:35:56 -0500

    make it possible to build variant images
    e.g:
        ./do.sh i386-min-dfsg
    builds an image with minimal files and without firmware-linux-nonfree.
    This image also happens to be small enough to fit on a CD.
    
    The variants supported are:
     -min           install fewer desktop apps
     -dfsg          don't install firmware-linux-nofree
     -l10n          install desktop localization for select languages
     -l10n-full     install desktop localization for all languages

--- do.sh
+++ do.sh
@@ -11,16 +11,58 @@ if [ -e env.sh ] ; then . ./env.sh ; fi
 
 if [ $# -eq 0 ] ; then set -- i386 amd64; fi
 for ARCH in "$@"; do
-    case "$ARCH" in
+    ARCH0="$ARCH"
+    case "${ARCH%%-*}" in
         i?86) KARCH=686-pae ;;
         *) KARCH="$ARCH" ;;
     esac
-    TARGET="$LOCATION/linuxcnc-stretch-uspace-$ARCH.iso"
+    TASK="task-xfce-desktop"
+    FIRMWARE="firmware-linux"
+    EXTRA="linuxcnc-uspace-dev linux-headers-rt-$KARCH \
+        hostmot2-firmware-all f-engrave mesaflash truetype-tracer"
+    while [ "${ARCH%-*}" != "${ARCH}" ]; do
+        case "$ARCH" in
+            *-dfsg)
+                ARCH=${ARCH%-dfsg};
+                FIRMWARE=firmware-linux-free
+            ;;
+            *-min)
+                ARCH=${ARCH%-min}
+                EXTRA=""
+                TASK="tasksel \
+                    xorg xserver-xorg-video-all xserver-xorg-input-all \
+                    desktop-base lightdm xfce4 xfce4-terminal mousepad \
+                    default-dbus-session-bus"
+            ;;
+            *-l10n)
+                ARCH=${ARCH%-l10n}
+                EXTRA="$EXTRA \
+                    task-german-desktop task-spanish-desktop \
+                    task-finnish-desktop task-hungarian-desktop \
+                    task-italian-desktop task-japanese-desktop \
+                    task-polish-desktop task-brazilian-portuguese-desktop \
+                    task-romanian-desktop task-russian-desktop \
+                    task-slovak-desktop task-serbian-desktop \
+                    task-swedish-desktop task-chinese-s-desktop \
+                    task-chinese-t-desktop"
+            ;;
+            *-l10n-full)
+                ARCH=${ARCH%-l10n-full}
+                TASK=live-task-xfce
+            ;;
+            *)
+                echo 1>&2 "Unknown variant requested: ${ARCH#*-}"
+                exit 1
+            ;;
+        esac
+    done
+    TARGET="$LOCATION/linuxcnc-stretch-uspace-$ARCH0.iso"
     lwr -o "$TARGET" \
         --architecture=$ARCH \
-        -t "live-task-base task-xfce-desktop" \
-        -e "linux-image-rt-$KARCH linux-headers-rt-$KARCH firmware-linux \
-            linuxcnc-uspace linuxcnc-uspace-dev hostmot2-firmware-all \
-            f-engrave hostmot2-firmware-all mesaflash truetype-tracer" \
-        --description="Unofficial LinuxCNC 'Stretch' $ARCH Live/Install"
+        -t "$TASK" \
+        -e "linux-image-rt-$KARCH \
+            $FIRMWARE \
+            linuxcnc-uspace
+            $EXTRA" \
+        --description="Unofficial LinuxCNC 'Stretch' $ARCH0 Live/Install"
 done
